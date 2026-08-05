c0e2d4cbda0a4e8083ccb4a16ddba8894ca72b54
jamiu654 <jamiu654@users.noreply.github.com>
2026-08-05 14:04:20 +0100

    configure postgreSQL for for Render

--- backend/deen_guide/settings.py
+++ backend/deen_guide/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,12 +51,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'deen_guide.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
