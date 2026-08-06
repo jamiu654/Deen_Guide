337f9dd558225c8faaa88fb77eca95245d6be7b1
jamiu654 <jamiu654@users.noreply.github.com>
2026-08-06 15:42:42 +0100

    Fix CORS and CSRF origins

--- backend/deen_guide/settings.py
+++ backend/deen_guide/settings.py
@@ -86,7 +86,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://deen-guide-frontend.onrender.com',
+    'https://deen-guide.onrender.com',
 ]
 CORS_ALLOW_CREDENTIALS = True
 SESSION_COOKIE_SAMESITE = 'None'
@@ -94,7 +94,7 @@ SESSION_COOKIE_SECURE = os.environ.get('DJANGO_COOKIE_SECURE', 'False') == 'True
 CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = os.environ.get('DJANGO_COOKIE_SECURE', 'False') == 'True'
 CSRF_TRUSTED_ORIGINS = [
-    'https://deen-guide-frontend.onrender.com',
+    'https://deen-guide.onrender.com',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
 ]
